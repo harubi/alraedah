7ad70879752e62c76025c50def605ef720d8dbca
harubi <936636+harubi@users.noreply.github.com>
2022-05-06 19:00:16 +0300

    Updated the DFS function to be iterative

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,4 @@
 from typing import *
-# This is very unsafe, but I did at for testing purposes.
-import sys
-sys.setrecursionlimit(1500)
 
 
 def get_lists_from_json(json: dict) -> list:
@@ -13,7 +10,7 @@ def get_lists_from_json(json: dict) -> list:
 
 
 def generate_adjacency_list(nodes: list) -> dict:
-    """ 
+    """
     A function to generate a linked-list representing a graph adjacency list from an array.
     For every item in the expanded list, we add to the graph the node as a key,
     and we add an array as a value containing the node (thus edge) pointed at.
@@ -31,19 +28,22 @@ def generate_adjacency_list(nodes: list) -> dict:
     return graph
 
 
-def dfs(visited: list, graph: dict, node: int):
-    """ 
-    DFS (Depth-first search) is our method here, where we recursively traverse 
-    out our graph, and save them into the visited list. I have extended the function here to
-    check if the working node is already in the visited list and if that list is also the 
+def dfs(graph: dict, starting_node: int):
+    """
+    DFS (Depth-first search) is our method here, where we iteratively traverse
+    out our graph, and save them into the visited list. I have extended the functionality here to
+    check if the working node is already in the visited list and if that list is also the
     same length as the graph, actually proving that we have, in fact, a perfect cycle ;)
     """
     try:
-        if node not in visited:
-            visited.append(node)
-            for neighbour in graph[node]:
-                return dfs(visited, graph, neighbour)
-        elif node in visited and len(visited) == len(graph):
+        visited, stack = [], [starting_node]
+        while stack:
+            node = stack.pop()
+            if node not in visited:
+                visited.append(node)
+                # add all unexplored, adjacent nodes to stack by using the list.extend method
+                stack.extend(set(graph[node]) - set(visited))
+        if len(visited) == len(graph) and visited[-1] == visited[starting_node]:
             return True
     except Exception as e:
         return e
@@ -56,14 +56,12 @@ def is_cyclic(json: dict) -> dict:
     A function that ties everything together and return true if the any graph is cyclic,
     It goes every list, converts it to a graph and check.
     """
-    results = {}
-    lists = get_lists_from_json(json)
+    results, lists = {}, get_lists_from_json(json)
     try:
         for list in lists:
-            visited = []
             graph = generate_adjacency_list(json[list])
-            firstGraphNode = next(iter(graph))
-            results[list] = dfs(visited, graph, firstGraphNode)
+            first_graph_node = next(iter(graph))
+            results[list] = dfs(graph, first_graph_node)
     except Exception as e:
         return e
 
